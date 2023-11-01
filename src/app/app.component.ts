31ad1725928ca91cb64276909b3a56b277317543
lapeko <lapeko88@gmail.com>
2023-11-02 00:07:17 +0100

    refactoring: simplify a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,24 +27,8 @@ export class AppComponent implements OnInit {
       this.store.dispatch(AuthActions.init());
 
       this.router.events.subscribe(event  => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.loading = true;
-            break;
-          }
-
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof NavigationError: {
-            this.loading = false;
-            break;
-          }
-          default: {
-            break;
-          }
-        }
+        this.loading = event instanceof NavigationStart;
       });
-
     }
 
     logout() {
